ba6c923fa46f3a23ed1e264e8eb2c4fcab7dfc1e
Oliver Smit <olmesm@gmail.com>
2022-10-10 16:46:25 +0100

    asdf-install script update

--- shell/asdf-install.sh
+++ shell/asdf-install.sh
@@ -3,20 +3,20 @@
 TOOL_FILE=".tool-versions"
 TOOLS=$(awk '{print $1}' $TOOL_FILE)
 
-echo "[info] Install all tools as per $TOOL_FILE\n"
+echo -e "[info] Install all tools as per $TOOL_FILE\n"
 
 for tool in $TOOLS; do
   asdf plugin add $tool
-  echo "---"
+  echo -e "---"
 done
 
-echo "[info] Completed\n"
+echo -e "[info] Completed\n"
 
-echo "[info] Install all tool versions as per $TOOL_FILE"
+echo -e "[info] Install all tool versions as per $TOOL_FILE"
 
-for tool in $(cat $TOOL_FILE); do
+for tool in $TOOLS; do
   asdf install $tool
-  echo "---"
+  echo -e "---"
 done
 
-echo "[info] Completed\n"
+echo -e "[info] Completed\n"
