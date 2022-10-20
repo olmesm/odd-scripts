cc5ce5c5be102ff7acc8f2c99583f248ae70524f
Oliver Smit <olmesm@gmail.com>
2022-10-20 12:22:45 +0100

    updated gcp

--- shell/gcp-terraform-service-account-create.sh
+++ shell/gcp-terraform-service-account-create.sh
@@ -1,26 +1,27 @@
 #!/usr/bin/env bash
 
-PROJECT_ID="$1"
-ACCOUNT_NAME=${2:-"terraform-service-account"}
+PARENT="$(basename $(pwd))"
+PROJECT_ID="${1:-$GCP_PROJECT}"
+ACCOUNT_NAME="${2:-"tf-sa-$PARENT"}"
 
 OUTPUT="tmp/service-account.json"
 
 mkdir -p $(dirname $OUTPUT)
 
-gcloud auth application-default login --project "$PROJECT_ID" --no-launch-browser
+gcloud auth application-default login --project $PROJECT_ID --no-launch-browser
 
-gcloud iam service-accounts create "$ACCOUNT_NAME" \
-  --project="$PROJECT_ID" \
+gcloud iam service-accounts create $ACCOUNT_NAME \
+  --project=$PROJECT_ID \
   --description="Terraform Service Account" \
   --display-name="Terraform Service Account"
 
-gcloud projects add-iam-policy-binding "$PROJECT_ID" \
-  --project="$PROJECT_ID" \
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+  --project=$PROJECT_ID \
   --member="serviceAccount:$ACCOUNT_NAME@$PROJECT_ID.iam.gserviceaccount.com" \
   --role="roles/editor"
 
 gcloud iam service-accounts keys create $OUTPUT \
-  --project="$PROJECT_ID" \
+  --project=$PROJECT_ID \
   --iam-account="$ACCOUNT_NAME@$PROJECT_ID.iam.gserviceaccount.com" \
   --key-file-type=json
 
--- shell/gcp-terraform-state-bucket-create.sh
+++ shell/gcp-terraform-state-bucket-create.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
 PARENT="$(basename $(pwd))"
-PROJECT_ID=$1
-BUCKET_NAME=${2:-$PARENT}
+PROJECT_ID="${1:-$GCP_PROJECT}"
+RANDOM_SUFFIX=$(echo "$RANDOM `date`" | md5sum | head -c 6)
+BUCKET_NAME="tf-state-${2:-$PARENT}-$RANDOM_SUFFIX"
 
-gsutil mb -p "$PROJECT_ID" "gs://$BUCKET_NAME"
+gsutil mb -p $PROJECT_ID gs://"$BUCKET_NAME"
+
+perl -i -pe "s|bucket *= *\".*\"|bucket = \"$BUCKET_NAME\"|" main.tf
