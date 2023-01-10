ab764fc16ae9a736942b02509cfab56f475c6448
Oliver Merrick Smit <olmesm@gmail.com>
2023-01-10 11:57:55 +0000

    corrected histo

--- shell/histor.sh
+++ shell/histor.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
+# histo 10 2 #=> returns history from line 10 with 2 lines above and below the command
+
 function histo () {
     LINE=$1
     COUNT=$2
 
-    AFTER=${COUNT} / 2
-    BEFORE=${LINE} - ${AFTER}
-
-    history | tail -${BEFORE} | head-${AFTER}
+    history | head -$(($LINE + $COUNT)) | tail -$(( 2 * $COUNT + 1))
 }
